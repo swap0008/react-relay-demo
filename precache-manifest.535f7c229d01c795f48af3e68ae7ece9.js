de577f9fbc843ee8234abd2d4819628827fd45e4
Swapnil Sharma <swap0008@gmail.com>
2019-06-21 21:33:02 +0530

    Updates

---
+++ precache-manifest.535f7c229d01c795f48af3e68ae7ece9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcf14bae7ff83973158c239f3db4b50",
+    "url": "/react-relay-demo/index.html"
+  },
+  {
+    "revision": "cd69eb7c76cbf64ae185",
+    "url": "/react-relay-demo/static/css/main.8a5d1ee3.chunk.css"
+  },
+  {
+    "revision": "4788bb385598e7d5231a",
+    "url": "/react-relay-demo/static/js/2.2757fa1b.chunk.js"
+  },
+  {
+    "revision": "cd69eb7c76cbf64ae185",
+    "url": "/react-relay-demo/static/js/main.21258917.chunk.js"
+  },
+  {
+    "revision": "b427ee5f8f8329fe5cf0",
+    "url": "/react-relay-demo/static/js/runtime~main.d7fd5236.js"
+  }
+]);
